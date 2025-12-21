c8fde933a682aac5f5b0cf82537eb31fc44a1b07
Lucky <r.matigorov@yandex.ru>
2025-12-22 00:42:31 +0300

    Add library

---
+++ ShoeShop/ShoeShop.Library/Models/OrderProduct.cs
@@ -0,0 +1,32 @@
+﻿namespace ShoeShop.Library.Models;
+
+/// <summary>
+/// Продукт заказа
+/// </summary>
+public partial class OrderProduct
+{
+    /// <summary>
+    /// Идентификатор заказа
+    /// </summary>
+    public int OrderId { get; set; }
+
+    /// <summary>
+    /// Артикль
+    /// </summary>
+    public string Article { get; set; } = null!;
+    
+    /// <summary>
+    /// Количество продуктов
+    /// </summary>
+    public byte Quantity { get; set; }
+
+    /// <summary>
+    /// Продукт
+    /// </summary>
+    public virtual Product ArticleNavigation { get; set; } = null!;
+
+    /// <summary>
+    /// Заказ
+    /// </summary>
+    public virtual Order Order { get; set; } = null!;
+}
---
+++ ShoeShop/ShoeShop.Library/Models/User.cs
@@ -0,0 +1,52 @@
+﻿namespace ShoeShop.Library.Models;
+
+/// <summary>
+/// Пользователь
+/// </summary>
+public partial class User
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public int UserId { get; set; }
+
+    /// <summary>
+    /// Фамилия
+    /// </summary>
+    public string Surname { get; set; } = null!;
+
+    /// <summary>
+    /// Имя
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Отчество
+    /// </summary>
+    public string? Patronymic { get; set; }
+
+    /// <summary>
+    /// Идентификатор роли
+    /// </summary>
+    public short RoleId { get; set; }
+
+    /// <summary>
+    /// Электронная почта
+    /// </summary>
+    public string Email { get; set; } = null!;
+
+    /// <summary>
+    /// Хэш пароля
+    /// </summary>
+    public string Passwordhash { get; set; } = null!;
+
+    /// <summary>
+    /// Заказы
+    /// </summary>
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    /// <summary>
+    /// Роль
+    /// </summary>
+    public virtual Role Role { get; set; } = null!;
+}
