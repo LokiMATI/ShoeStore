382ebd30dd32f5058e17991f23e20f17c82022dd
Lucky <r.matigorov@yandex.ru>
2025-12-23 01:53:03 +0300

    Configure Program.cs

--- ShoeShop/ShoeShop.API/Program.cs
+++ ShoeShop/ShoeShop.API/Program.cs
@@ -1,3 +1,6 @@
+using ShoeShop.Library.Contexts;
+using ShoeShop.Library.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ShoeDbContext>();
+
+builder.Services.AddScoped<ProductService>();
 
 var app = builder.Build();
 
